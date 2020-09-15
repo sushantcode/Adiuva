e80e01387113254a578db8beb3c467265601ece9
Sushant Gupta <sushantgupta@Sushants-MacBook-Pro.local>
2020-09-15 16:56:24 -0500

    Project modified

---
+++ helpme/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="gs://helpme-b7e48.appspot.com/images/logo.png" />
+        <p>
+          Welcome to HelpMe.com!!!
+        </p>
+          Website under construction. Keep looking for more information.
+      </header>
+    </div>
+  );
+}
+
+export default App;
