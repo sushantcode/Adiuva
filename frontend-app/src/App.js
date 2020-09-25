87e7110fa4cf1dca083e31e7b61f9790cd4643c7
Sushant Gupta <sushantgupta@Sushants-MacBook-Pro.local>
2020-09-25 17:08:07 -0500

    refactor

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
+import WelcomeNavbar from "./components/WelcomeNavbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+    return (
+        <>
+            <Router>
+                <WelcomeNavbar />
+                <Switch>
+                    <Route path="/" exact />
+                </Switch>
+            </Router>
+        </>
+    );
 }
 
 export default App;
