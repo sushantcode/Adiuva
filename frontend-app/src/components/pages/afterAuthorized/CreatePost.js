7afd6dccef3051df9e8a00e245c6e5b711202ff5
Sushant Gupta <sushantgupta@Sushants-MacBook-Pro.local>
2020-10-19 01:34:18 -0500

    create post

--- frontend-app/src/components/pages/afterAuthorized/CreatePost.js
+++ frontend-app/src/components/pages/afterAuthorized/CreatePost.js
@@ -1,14 +1,45 @@
 import React from "react";
 import Navbar from "../../MainNavbar";
+import "./CreatePost.css";
 
 function CreatePost() {
   return (
     <><Navbar />
     <div className="contact-card">
-    <p>Create Post Page</p>
-    <p> Create your post here...
-        
-    </p>
+    <div className="content-box">
+        <div className="content">
+          <h1> Creating New Post </h1>
+            <div className="radio1">
+                <form>
+                <input type="radio" id="postRequest" name="postType" value="option1" /> Request
+                <input type="radio" id="postDonate" name="postType" value="option2" /> Donate
+                </form>
+            </div>
+
+            <div className="zipcode">
+                <input type="text" id="zipcode" name="zipCode" placeholder=" Enter zipcode*" />
+            </div>
+
+            <div className="description-input">
+              <label htmlFor="description" className="form-label">
+                <input
+                  id="description"
+                  type="description"
+                  name="description"
+                  className="description-input"
+                  placeholder="Please Describe what you are posting........"
+                  />
+              </label>
+            
+            </div>
+              <button className="btnn" type="upload">
+                Up Load Image
+              </button>
+              <button className="form-btnn" type="post">
+                Post
+              </button>
+        </div>
+      </div>
     </div>
     </>
   );
