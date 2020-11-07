22dcb7ed9bfa73970941447f6af571f2f19bae5b
Sushant Gupta <sushantgupta@Sushants-MacBook-Pro.local>
2020-11-06 21:35:51 -0600

    createpost backed done

--- frontend-app/src/components/pages/afterAuthorized/CreatePost.js
+++ frontend-app/src/components/pages/afterAuthorized/CreatePost.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import Navbar from "../../MainNavbar";
 import "./CreatePost.css";
-import app from '../../utils/fireApp';
+import app, { storage, db } from '../../utils/fireApp';
 
 const CreatePost = ({ history }) => {
   
@@ -11,41 +11,134 @@ const CreatePost = ({ history }) => {
         history.push("/login");
     };
 
+    const [img, setImg] = useState(null);
+    let imgUrl = '';
+    const [postType, setPostType] = useState('');
+
+    const fileHandler = data => {
+        if(data.target.files[0]) {
+            setImg(data.target.files[0]);
+        }
+    };
+
+    const onSubmitHandler = useCallback(
+        async event => {
+            event.preventDefault();
+
+            const {
+                body,
+                zipCode
+            } = event.target.elements;
+
+            let errors = '';
+
+            if (postType === '') {
+                errors = 'Post-type must be selected.';
+            }
+
+            else if (zipCode.value.trim() === '') {
+                errors = 'Zipcode must not be empty.';
+            }
+
+            if (errors !== '') {
+                alert(errors);
+            }
+            else {
+                try {
+                    const uploadToFirebase = storage.ref(`postImages/${img.name}`).put(img);
+                    uploadToFirebase.on(
+                        "state_changed",
+                        snapshot => {},
+                        error => {
+                            alert(error);
+                        },
+                        () => {
+                            storage
+                                .ref("postImages")
+                                .child(img.name)
+                                .getDownloadURL()
+                                .then(Url => {
+                                    const userID = app.auth().currentUser.uid;
+                                    db.collection("users").doc(userID).get()
+                                    .then((doc) => {
+                                        const userName = doc.data().fName + ' ' + doc.data().lName;
+                                        const newPost = {
+                                            body: body.value,
+                                            createdAt: new Date().toISOString(),
+                                            imgURL: Url,
+                                            postType: postType,
+                                            userID: userID,
+                                            userName: userName,
+                                            zipcode: zipCode.value
+                                        };
+                                        // Pushing the user information once user is signed up successfully to database
+                                        db.collection("dPosts")
+                                        .add(newPost)
+                                        .then(() => {
+                                            history.push("/feed");
+                                            alert("Post has been created successfully.");
+                                        })
+                                        .catch((err) => {
+                                            alert(err);
+                                        })
+                                    })
+                                })
+                                .catch((err) => {
+                                    alert(err)
+                                });
+                        }
+                    );
+                    
+                } catch (err) {
+                    alert (err);
+                }
+            }
+
+        }
+    );
+
+
     return (
     <><Navbar />
     <div className="contact-card">
     <div className="content-box">
         <div className="content">
+        <form onSubmit={onSubmitHandler}>
           <h1> Creating New Post </h1>
             <div className="radio1">
-                <form>
-                <input type="radio" id="postRequest" name="postType" value="option1" /> <b>Request</b>
-                <input type="radio" id="postDonate" name="postType" value="option2" /> <b>Donate</b>
-                </form>
+                <p><h4> *Please select one of the following options: </h4>
+                <input type="radio" name="postType" value="Donation Request"
+                 checked={postType === "Donation Request"} 
+                 onChange={(e) => {setPostType(e.target.value)}} /> <b>Donation Request</b>
+                
+                <input type="radio" id="postDonate" name="postType"
+                 value="Donation Offered" 
+                 checked={postType === "Donation Offered"} 
+                 onChange={(e) => {setPostType(e.target.value)}} /> <b>Donation Offered</b>
+                </p>
             </div>
 
             <div className="zipcode">
-                <input type="text" id="zipcode" className="zipCode" placeholder=" Enter zipcode*" />
+                <input type="number" id="zipCode" className="zipCode" placeholder=" Enter zipcode*" />
             </div>
 
             <div className="description-input">
-              <label htmlFor="description" className="form-label">
+              <label htmlFor="body" className="form-label">
                 <input
-                  id="description"
-                  type="description"
-                  name="description"
-                  className="description-input"
-                  placeholder="Please Describe what you are posting........"
+                  id="body"
+                  type="body"
+                  className="body"
+                  placeholder="Please Describe what you are posting about........"
                   />
               </label>
-            
             </div>
-              <button className="btnn" type="upload">
-                Up Load Image
-              </button>
+              <div>
+              <input className="uploadMenu" type="file" onChange={fileHandler} accept="image/png, image/jpeg" />
+              </div>
               <button className="form-btnn" type="post">
                 Post
               </button>
+              </form>
         </div>
       </div>
     </div>
