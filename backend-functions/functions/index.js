4001a0d847e857b74c7b4a9e72d59cd4fc633330
Sushant <58162933+sushantcode@users.noreply.github.com>
2021-03-09 00:17:29 -0600

    Update index.js

--- backend-functions/functions/index.js
+++ backend-functions/functions/index.js
@@ -9,14 +9,14 @@ const app = require("express")();
 fire_admin.initializeApp();
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA2mQWzTiRkzq8VxQxbpAxEpnjLePQo4Z8",
-    authDomain: "helpme-b7e48.firebaseapp.com",
-    databaseURL: "https://helpme-b7e48.firebaseio.com",
-    projectId: "helpme-b7e48",
-    storageBucket: "helpme-b7e48.appspot.com",
-    messagingSenderId: "112348268808",
-    appId: "1:112348268808:web:99e8308defcfa614d404bd",
-    measurementId: "G-EN26NVPQ60"
+    apiKey: "PROJECT_API_KEY_REMOVED_FOR_SECURITY_REASON",
+    authDomain: "INFO_REMOVED_FOR_SECURITY_REASON",
+    databaseURL: "INFO_REMOVED_FOR_SECURITY_REASON",
+    projectId: "INFO_REMOVED_FOR_SECURITY_REASON",
+    storageBucket: "INFO_REMOVED_FOR_SECURITY_REASON",
+    messagingSenderId: "INFO_REMOVED_FOR_SECURITY_REASON",
+    appId: "INFO_REMOVED_FOR_SECURITY_REASON",
+    measurementId: "INFO_REMOVED_FOR_SECURITY_REASON"
   };
 
 
